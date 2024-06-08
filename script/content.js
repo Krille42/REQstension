a097fc67ada10b5e457c2b93c724335732ec3363
Krille <krille96@me.com>
2024-06-08 07:33:53 +0200

    Status element prepended instead of appended

--- script/content.js
+++ script/content.js
@@ -85,8 +85,8 @@
 
       // Show status in injected element
       let statusEl = document.createElement("p")
-      statusEl.style = "display: absolute; top: 1em; width: min-content; margin: auto; padding: 1em; background-color: #666666;"
-      document.body.appendChild(statusEl)
+      statusEl.style = "display: fixed; width: max-content; top: 1em; margin: auto; padding: 1em; background-color: #666666; z-index: 10;"
+      document.body.prepend(statusEl)
 
       if(window.location.href.includes("https://volvoitsm.service-now.com/com.glideapp.servicecatalog_cat_item_view.do?v=1&sysparm_id=e7351f31dbb0a8109507a155059619a3")) {
 
@@ -201,6 +201,12 @@
                               }, 200);
                               
                         }, 200);
+
+                        statusEl.innerHTML = "Task ready for closure"
+
+                  }else {
+
+                        statusEl.innerHTML = "Task closed"
 
                   }
 
